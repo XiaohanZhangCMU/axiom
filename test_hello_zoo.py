d1e75181bf28ecc6a2815dfa5eb6ae163c0f8e99
Xiaohan Zhang <xiaohanzhang.cmu@gmail.com>
2017-07-21 10:58:40 -0700

    hello zoo success (but need to exclude hello_world.cpp)

---
+++ test_hello_zoo.py
@@ -0,0 +1,14 @@
+import axiom
+
+assert 'hello' in dir(axiom)
+assert callable(axiom.hello)
+
+print(axiom.hello())
+animal = axiom.Animal("dog")
+
+print(animal)
+print("The C++ object is at 0x{0}".format(animal.get_address()))
+print("I see a {0}".format(animal.name))
+
+animal.name = "cat"
+print("I see a {0}".format(animal.name))
