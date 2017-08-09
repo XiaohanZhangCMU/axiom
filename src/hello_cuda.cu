55e740f214f04601015b0a1c0dd44735dddaef41
Xiaohan Zhang <xiaohanzhang.cmu@gmail.com>
2017-08-08 18:24:33 -0700

    Rewrite test_axiom.py structurally

--- src/hello_cuda.cu
+++ src/hello_cuda.cu
@@ -6,7 +6,7 @@ __global__ void saxpy(int n, float a, float *x, float *y)
 {
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   if (i < n) y[i] = a*x[i] + y[i];
-  printf("In GPU: n= %d: [%d,%d,%d]\n", n, blockIdx.x, blockDim.x, threadIdx.x);
+//  printf("In GPU: n= %d: [%d,%d,%d]\n", n, blockIdx.x, blockDim.x, threadIdx.x);
 }
 
 int CudaAnimal::test_saxpy(void)
--- test_axiom.py
+++ test_axiom.py
@@ -3,64 +3,82 @@ import numpy as np
 
 '''
 Remarks to be noted for users:
-
-    Sets the mode. It is recommended that you don't change the mode halfway
-    into the program since that may cause allocation of pinned memory being
-    freed in a non-pinned way, which may cause problems - I haven't verified
-    it personally but better to note it here in the header file.
-
-Todo: 
-    compile .....
+    1) Sets the mode. It is recommended that you don't change the mode halfway
+       into the program since that may cause allocation of pinned memory being
+       freed in a non-pinned way, which may cause problems - I haven't verified
+       it personally but better to note it here in the header file.
 '''
 
 from lib import axiom
-'''
-Test class Animal
-'''
-animal = axiom.Animal("dog")
-print(animal)
-print("The Animal is at 0x{0}".format(animal.get_address()))
-print("I see a {0}".format(animal.name))
-animal.name = "cat"
-print("I see a {0}".format(animal.name))
 
-'''
-Test class CudaAnimal
-'''
-cuda_animal = axiom.CudaAnimal("dog")
-cuda_animal.test_saxpy()
+def test_Animal():
+    animal = axiom.Animal("dog")
+    print(animal)
+    print("The Animal is at 0x{0}".format(animal.get_address()))
+    print("I see a {0}".format(animal.name))
+    animal.name = "cat"
+    print("I see a {0}".format(animal.name))
+    return 1
 
-'''
-Test class NumpyAnimal
-'''
-numpy_animal = axiom.NumpyAnimal()
-dim1 = 10
-dim2 = 1
-x = np.random.random((dim1, dim2)).astype(np.float64)
-print(np.sum(x * x))
-print(np.sum(axiom.square_matrix(x)))
+def test_CudaAnimal():
+    cuda_animal = axiom.CudaAnimal("dog")
+    cuda_animal.test_saxpy()
+    return 1
 
-'''
-Test Axiom
-'''
-axiom.set_mode_cpu()
-axiom.set_mode_gpu()
-axiom.set_multiprocess(True)
+def test_NumpyAnimal():
+    numpy_animal = axiom.NumpyAnimal()
+    dim1 = 10
+    dim2 = 1
+    x = np.random.random((dim1, dim2)).astype(np.float64)
+    print(np.sum(x * x))
+    print(np.sum(axiom.square_matrix(x)))
+    return 1
 
-'''
-Test Tensor
-'''
-t = axiom.Tensor()
-print("tensor see {0}".format(t.count))
-m = t.L2
-print("tensor see {0}".format(m))
+def test_Axiom():
+    axiom.set_mode_cpu()
+    axiom.set_mode_gpu()
+    axiom.set_multiprocess(True)
+    return 1
 
-t.reshape(1)
-'''
-Test SyncedMemory
-'''
+def test_Tensor():
+    t = axiom.Tensor()
+    print("tensor see {0}".format(t.count))
+    m = t.L2
+    print("tensor see {0}".format(m))
+    t.reshape(1)
+    return 1
 
-'''
-Test class Axiom
-'''
-#axiom.set_mode_cpu()
+def test_MemOps():
+    t = axiom.Tensor()
+    print("test is not implemented")
+    return 0
+    foo = t.gpu_data(); # data copied cpu->gpu.
+    foo = t.cpu_data(); # no data copied since both have up-to-date contents.
+    bar = t.mutable_gpu_data(); # no data copied.
+    # ... some operations ...
+    bar = t.mutable_gpu_data(); # no data copied when we are still on GPU.
+    foo = t.cpu_data(); # data copied gpu->cpu, since the gpu side has modified the data
+    foo = t.gpu_data(); # no data copied since both have up-to-date contents
+    bar = t.mutable_cpu_data(); # still no data copied.
+    bar = t.mutable_gpu_data(); # data copied cpu->gpu.
+    bar = t.mutable_cpu_data(); # data copied gpu->cpu.
+    print("test is not implemented")
+    return 0
+
+def main(argv):
+    dict_tests = {}
+    dict_tests['test_animal'] = test_Animal()
+    dict_tests['test_CudaAnimal'] = test_CudaAnimal()
+    dict_tests['test_NumpyAnimal']= test_NumpyAnimal()
+    dict_tests['test_Axiom'] = test_Axiom()
+    dict_tests['test_Tensor']= test_Tensor()
+    dict_tests['test_MemOps']= test_MemOps()
+  
+    for key in dict_tests:
+        if dict_tests[key] == 1:
+            print("{0} pass test.".format(key))
+        else:
+            print("{0} fail test.".format(key))
+
+if __name__ == "__main__":
+     main(sys.argv[1:])
