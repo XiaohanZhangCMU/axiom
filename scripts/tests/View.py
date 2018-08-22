9ceef8964b8497462c6874496b92f2c22ec13ef2
Xiaohan Zhang <xiaohanzhang.cmu@gmail.com>
2018-08-21 22:00:45 -0700

    Add View.py to scripts/tests/ and work well with test-si-01.py. It use PyOpenGL to visualize two spheres. Reacting to simple keyboard and mouse inputs

---
+++ scripts/tests/View.py
@@ -0,0 +1,219 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+class Viewer(object):
+    def __init__(self, sim, width, height, display=None):
+        #-----------
+        # VARIABLES
+        #-----------
+
+        self.g_fViewDistance = 9.
+        self.g_Width = 600   # Fix this to take args
+        self.g_Height = 600  # Fix this to take args
+
+        self.g_nearPlane = 1.
+        self.g_farPlane = 1000.
+
+        self.action = ""
+        self.xStart = self.yStart = 0.
+        self.zoom = 65.
+
+        self.xRotate = 0.
+        self.yRotate = 0.
+        self.zRotate = 0.
+
+        self.xTrans = 0.
+        self.yTrans = 0.
+
+        #self.SR = sim.SR
+        #self.H = sim.H
+
+    #-------------------
+    # SCENE CONSTRUCTOR
+    #-------------------
+
+    def scenemodel(self):
+        glRotate(90,0.,0.,1.)
+        glutSolidTeapot(1.)
+
+    def sphere(self):
+        glPushMatrix(self)
+        color = [1.0,0.,0.,1.]
+        glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
+        glutSolidSphere(2,20,20)
+        glPopMatrix()
+
+    def spheres(self):
+        glPushMatrix()
+        color = [1.0,0.,0.,1.]
+        glTranslated(-1.2,-1.2,-1.2);
+        glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
+        glutSolidSphere(0.2,20,20)
+        glPopMatrix()
+
+        glPushMatrix()
+        color = [1.0,0.,0.,1.]
+        glTranslated(1.2,1.2,1.2);
+        glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
+        glutSolidSphere(0.2,20,20)
+        glPopMatrix()
+
+    #--------
+    # VIEWER
+    #--------
+
+    def printHelp(self):
+        print("""\n\n
+             -------------------------------------------------------------------\n
+             Left Mousebutton       - move eye position (+ Shift for third axis)\n
+             Middle Mousebutton     - translate the scene\n
+             Right Mousebutton      - move up / down to zoom in / out\n
+             key (r)                - reset viewpoint\n
+             key (z)                - zoom in (not working yet)\n
+             Key (q)                - exit the viewer\n
+             -------------------------------------------------------------------\n
+             \n""")
+
+
+    def init(self):
+        glEnable(GL_NORMALIZE)
+        glLightfv(GL_LIGHT0,GL_POSITION,[ .0, 10.0, 10., 0. ] )
+        glLightfv(GL_LIGHT0,GL_AMBIENT,[ .0, .0, .0, 1.0 ]);
+        glLightfv(GL_LIGHT0,GL_DIFFUSE,[ 1.0, 1.0, 1.0, 1.0 ]);
+        glLightfv(GL_LIGHT0,GL_SPECULAR,[ 1.0, 1.0, 1.0, 1.0 ]);
+        glEnable(GL_LIGHT0)
+        glEnable(GL_LIGHTING)
+        glEnable(GL_DEPTH_TEST)
+        glDepthFunc(GL_LESS)
+        glShadeModel(GL_SMOOTH)
+        self.resetView()
+
+
+    def resetView(self):
+        #global zoom, xRotate, yRotate, zRotate, xTrans, yTrans
+        self.zoom = 65.
+        self.xRotate = 0.
+        self.yRotate = 0.
+        self.zRotate = 0.
+        self.xTrans = 0.
+        self.yTrans = 0.
+        glutPostRedisplay()
+
+
+    def display(self):
+        # Clear frame buffer and depth buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        # Set up viewing transformation, looking down -Z axis
+        glLoadIdentity()
+        gluLookAt(0, 0, -self.g_fViewDistance, 0, 0, 0, -.1, 0, 0)   #-.1,0,0
+        # Set perspective (also zoom)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(self.zoom, float(self.g_Width)/float(self.g_Height), self.g_nearPlane, self.g_farPlane)
+        glMatrixMode(GL_MODELVIEW)
+        # Render the scene
+        self.polarView()
+
+        #scenemodel()
+        #sphere()
+        self.spheres()
+
+        # Make sure changes appear onscreen
+        glutSwapBuffers()
+
+
+    def reshape(self, width, height):
+        #global g_Width, g_Height
+        self.g_Width = width
+        self.g_Height = height
+        glViewport(0, 0, self.g_Width, self.g_Height)
+
+
+    def polarView(self):
+        glTranslatef( self.yTrans/100., 0.0, 0.0 )
+        glTranslatef(  0.0, -self.xTrans/100., 0.0)
+        glRotatef( -self.zRotate, 0.0, 0.0, 1.0)
+        glRotatef( -self.xRotate, 1.0, 0.0, 0.0)
+        glRotatef( -self.yRotate, .0, 1.0, 0.0)
+
+
+    def keyboard(self, key, x, y):
+        global zTr, yTr, xTr
+        if(key==b'r'): self.resetView()
+        if(key==b'z'): self.zoomin()
+        if(key==b'q'): exit(0)
+        glutPostRedisplay()
+
+
+    def mouse(self, button, state, x, y):
+        #global action, xStart, yStart
+        if (button==GLUT_LEFT_BUTTON):
+            if (glutGetModifiers() == GLUT_ACTIVE_SHIFT):
+                self.action = "MOVE_EYE_2"
+            else:
+                self.action = "MOVE_EYE"
+        elif (button==GLUT_MIDDLE_BUTTON):
+            self.action = "TRANS"
+        elif (button==GLUT_RIGHT_BUTTON):
+            self.action = "ZOOM"
+        self.xStart = x
+        self.yStart = y
+
+
+    def motion(self, x, y):
+        #global zoom, xStart, yStart, xRotate, yRotate, zRotate, xTrans, yTrans
+        if (self.action=="MOVE_EYE"):
+            self.xRotate += x - self.xStart
+            self.yRotate -= y - self.yStart
+        elif (self.action=="MOVE_EYE_2"):
+            self.zRotate += y - self.yStart
+        elif (self.action=="TRANS"):
+            self.xTrans += x - self.xStart
+            self.yTrans += y - self.yStart
+        elif (self.action=="ZOOM"):
+            self.zoom -= y - self.yStart
+            if self.zoom > 150.:
+                self.zoom = 150.
+            elif self.zoom < 1.1:
+                self.zoom = 1.1
+        else:
+            print("unknown action\n", action)
+        self.xStart = x
+        self.yStart = y
+        glutPostRedisplay()
+
+    def zoomin(self):
+        scale_fac = 1.1
+        glPushMatrix()
+        glScalef(scale_fac,scale_fac,scale_fac)
+        glPopMatrix()
+        #self.zoom -= 0 - self.yStart
+        #if self.zoom > 150.:
+        #    self.zoom = 150.
+        #elif self.zoom < 1.1:
+        #    self.zoom = 1.1
+        glutPostRedisplay()
+
+
+    #------
+    # MAIN
+    #------
+    def rendering(self):
+        # GLUT Window Initialization
+        glutInit()
+        glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB| GLUT_DEPTH)      # zBuffer
+        glutInitWindowSize (self.g_Width,self.g_Height)
+        glutInitWindowPosition (0 + 4, int(self.g_Height / 4) )
+        glutCreateWindow ("Visualizzatore_2.0")
+        # Initialize OpenGL graphics state
+        self.init ()
+        # Register callbacks
+        glutReshapeFunc(self.reshape)
+        glutDisplayFunc(self.display)
+        glutMouseFunc(self.mouse)
+        glutMotionFunc(self.motion)
+        glutKeyboardFunc(self.keyboard)
+        self.printHelp()
+        # Turn the flow of control over to GLUT
+        glutMainLoop()
--- scripts/tests/test-si-01.py
+++ scripts/tests/test-si-01.py
@@ -1,34 +1,79 @@
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GL import *
 import sys
+
 sys.path.append('/Users/x/Downloads/GraphicsScratch/axiom/lib/')
 import mdsw
+from View import Viewer
 
+""" Conjugate-Gradient relaxation """
 def relax_fixbox(obj,relaxsteps):
-    #Conjugate-Gradient relaxation
     obj.conj_ftol = 1e-7
     obj.conj_itmax = 1000
     obj.conj_fevalmax = relaxsteps
     obj.conj_fixbox = 1
     obj.relax()
 
-def open_window(obj):
-    obj.atomradius = mdsw.VectorDouble([0.67])
-    obj.bondradius = 0.3
-    obj.bondlength = 2.8285 #for Si
-    obj.atomcolor = [ "orange"]
-    obj.highlightcolor = "purple"
-    obj.bondcolor = "red"
-    obj.backgroundcolor = "gray70"
-    obj.plotfreq = 1
-    obj.rotateangles = mdsw.VectorDouble([0,0,0,1.25])
-    obj.plot_limits = mdsw.VectorDouble([  1, -10 , 10 , -10 , 10 , 0.15 , 0.5  ])
-    obj.refreshnnlist
-    obj.openwin()
-    obj.alloccolors
-    obj.rotate
-    obj.saverot
-    obj.plot_atom_info = 0
-    obj.plot_color_windows=mdsw.VectorDouble([1, -10, 10])
-    obj.plot()
+#def open_window(obj):
+#    obj.atomradius = mdsw.VectorDouble([0.67])
+#    obj.bondradius = 0.3
+#    obj.bondlength = 2.8285 #for Si
+#    obj.atomcolor = [ "orange"]
+#    obj.highlightcolor = "purple"
+#    obj.bondcolor = "red"
+#    obj.backgroundcolor = "gray70"
+#    obj.plotfreq = 1
+#    obj.rotateangles = mdsw.VectorDouble([0,0,0,1.25])
+#    obj.plot_limits = mdsw.VectorDouble([  1, -10 , 10 , -10 , 10 , 0.15 , 0.5  ])
+#    obj.refreshnnlist
+#    obj.openwin()
+#    obj.alloccolors
+#    obj.rotate
+#    obj.saverot
+#    obj.plot_atom_info = 0
+#    obj.plot_color_windows=mdsw.VectorDouble([1, -10, 10])
+#    obj.plot()
+
+#def render(name):
+#    glutInit(sys.argv)
+#    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+#    glutInitWindowSize(400,400)
+#    glutCreateWindow(name)
+#
+#    glClearColor(0.,0.,0.,1.)
+#    glShadeModel(GL_SMOOTH)
+#    glEnable(GL_CULL_FACE)
+#    glEnable(GL_DEPTH_TEST)
+#    glEnable(GL_LIGHTING)
+#    lightZeroPosition = [10.,4.,10.,1.]
+#    lightZeroColor = [0.8,1.0,0.8,1.0] #green tinged
+#    glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
+#    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
+#    glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
+#    glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
+#    glEnable(GL_LIGHT0)
+#    glutDisplayFunc(display)
+#    glMatrixMode(GL_PROJECTION)
+#    gluPerspective(40.,1.,1.,40.)
+#    glMatrixMode(GL_MODELVIEW)
+#    gluLookAt(0,0,10,
+#              0,0,0,
+#              0,1,0)
+#    glPushMatrix()
+#    glutMainLoop()
+#    return
+#
+#def display():
+#    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+#    glPushMatrix()
+#    color = [1.0,0.,0.,1.]
+#    glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
+#    glutSolidSphere(2,20,20)
+#    glPopMatrix()
+#    glutSwapBuffers()
+#    return
+
 
 """ Main Program Starts
 """
@@ -43,7 +88,10 @@ sw.finalcnfile = "relaxed.cn"
 sw.writecn(0,False)
 sw.eval()
 
-open_window(sw)
+#render("mdsw")
+#open_window(sw)
+view = Viewer(sw, 600, 600)
+view.rendering()
 sw.sleep()
 relax_fixbox(sw,1000)
 
