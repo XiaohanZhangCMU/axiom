1fa9486bb92d15eb2adfb8263b57303eda3a4865
Xiaohan Zhang <xiaohanzhang.cmu@gmail.com>
2019-04-22 22:01:27 -0700

    Update solve_lorenz_pde.py. Work in progress

--- python/lorenz/solve_lorenz_pde.py
+++ python/lorenz/solve_lorenz_pde.py
@@ -1,61 +1,64 @@
-""" Use tensorflow to solve PDE of lorenz-G equation
+"""
+    Use tensorflow to solve PDE of lorenz-G equation
     1) Formulate least square loss
     2) Sample PDE domain patchwise
+
+    Questions to resolve:
+    1) Run time shape dimension does not match?
+    2) Normalize input patch to have values [-1,1] for each channel?
+    3) Add laplacian regularizer to PDE.
 """
 import sys
-sys.path.append('../../lib/')
 from termcolor import colored, cprint
-import lorenz
+import numpy as np
 import tensorflow as tf
 
-def fc(x, scope, nh, *, init_scale=1.0, init_bias=0.0):
-    with tf.variable_scope(scope):
-        nin = x.get_shape()[1].value
-        w = tf.get_variable("w", [nin, nh], initializer=ortho_init(init_scale))
-        b = tf.get_variable("b", [nh], initializer=tf.constant_initializer(init_bias))
-        return tf.matmul(x, w)+b
 
-def mlp(num_layers=2, num_hidden=64, activation=tf.tanh, layer_norm=False):
-    def network_fn(X):
-        h = tf.layers.flatten(X)
-        for i in range(num_layers):
-            h = fc(h, 'mlp_fc{}'.format(i), nh=num_hidden, init_scale=np.sqrt(2))
-            h=activation(h)
-        return h
-    return network_fn
+def solve_lorenz_pde(xmin, xmax, zmin, zmax, epsilon, sigma, beta, gamma):
+    sub_nx, sub_nz = 16, 16
+    patch_x, patch_z = 1., 1.
 
-def solve_lorenz_pde(xmin, xmax, zmin, zmax, epsilon):
-    sub_nx, sub_nz = 10, 10
-    
-    x_ph = tf.placeholder(shape=(None, sub_nx, sub_nz,2),name='input')
-    model = mlp(num_layers=2,num_hidden=32)
+    x_ph = tf.placeholder(dtype=np.float64, shape=(None, sub_nx, sub_nz, 2), name='input')
+    model = mlp(num_layers=2, num_hidden=32)
 
-    def sample_domain(batch=32):
-        xs = uniform(xmin, xmax, batch)
-        zs = uniform(zmin, zmax, batch)
-        xn = linspace(xs, xs+patch_size_x, sub_nx)
-        zn = linspace(zs, zs+patch_size_z, sub_nz)
-        return np.concat(xn,zn,axis=1)
+    def mlp(x, hidden_sizes=(32,), activation=tf.tanh, output_activation=None):
+        for h in hidden_sizes[:-1]:
+            x = tf.layers.dense(x, units=h, activation=activation)
+        return tf.layers.dense(x, units=hidden_sizes[-1], activation=output_activation)
+
+    def sample_domain(batch_size=32):
+        xs = np.random.uniform(xmin, xmax, batch_size)
+        zs = np.random.uniform(zmin, zmax, batch_size)
+        return np.array([np.stack(np.meshgrid(np.linspace(xi-0.5*patch_x, xi+0.5*patch_x, sub_nx),
+            np.linspace(zi-0.5*patch_z, zi+0.5*patch_z, sub_nz)), axis=-1)
+            for (xi,zi) in zip(xs,zs)])
 
     def loss_function(inputs):
         xadv = tf.identity(inputs) # Such that dG/d_xadv = dG/d_x
-        G = model.netwrok_fn(xadv)
-        dG,_ = tf.gradients(G, xs=xadv)
-        # d2G,_ = tf.hessians(G, xs=xadv) # Regularizer
-        loss_sum = tf.reduce_mean(tf.math.square( 
-            	tf.math.scalar_mul(sigma, tf.math.multiply((G-xadv), Gx)) 
-            	+ tf.math.multiply((tf.math.scalar_mul(x,G)-tf.math.scalar_mul(beta,z)),Gz)  
-                + G  
-                + tf.math.multiply(x, (z-gamma)) 
-                # + tf.math.scalar_mul(epsilon,(d2Gx+d2Gz))  # Regularizer
-                ))
-        return loss_sum
+        G = mlp(xadv, hidden_sizes=[32,1])
+        dG = tf.gradients(G, xadv)[0]
+        # d2G,_ = tf.hessians(G, xadv) # Regularizer
+        print('G and dG shape')
+        print(xadv.shape)
+        print(G.shape)
+        print(dG.shape)
+
+        loss_sum = tf.reduce_mean(((sigma*((G-xadv[:,0])*dG[:,0])) +
+                                   (((xadv[:,0]*G)-beta*xadv[:,1])*dG[:,1]) +
+                                   G +
+                                   (xadv[:,0]*(xadv[:,1]-gamma))
+                                    # + tf.math.scalar_mul(epsilon,(d2Gx+d2Gz))  # Regularizer
+                                   )**2)
+        print(loss_sum.shape)
+        return loss_sum, G
 
     """ Optimization setup
     """
     lr = 1e-3  # learning rate
     tol = 1e-9 # convergence tol
+    batch_size = 1<<5
     epochs = 1<<20
+
     optimizer = tf.train.AdamOptimizer(lr)
     grads_and_vars = optimizer.compute_gradients(loss_sum)
     op = optimizer.apply_gradients(grads_and_vars,global_step=tf.Variable(0, trainable=False))
@@ -71,14 +74,14 @@ def solve_lorenz_pde(xmin, xmax, zmin, zmax, epsilon):
         np.random.shuffle(ind)
 
         for batch in range(n_batch):
-            start = batch * batch_size
-            end = min(n_sample, start+batch_size) 
+            X_sample = sample_domain(batch_size)
             # Feed X to x_ph, do optimization
-            result = sess.run([op, loss_sum, G], feed_dict={x_ph:X_data[start:end]})
+            batch_loss = loss_function(x_ph)
+            result = sess.run([op, batch_loss, G], feed_dict={x_ph:X_sample})
 
         if epoch % 100 == 0:
             print("Epoch = {:5d}; Residuals={: 5.10E};".format(epoch, result[1]))
-            # Save model weights 
+            # Save model weights
             # ...
         if err < 1e-9:
             cprint("Converged in {:d} epochs!!!".format(epoch), 'green', 'on_red')
@@ -86,3 +89,9 @@ def solve_lorenz_pde(xmin, xmax, zmin, zmax, epsilon):
             return result[-1]
 
     cprint("Failed to converged in {:d} epochs!!!".format(epoch), 'green', 'on_red')
+
+if __name__ == '__main__':
+    solve_lorenz_pde(xmin=-10, xmax=10, zmin=-10, zmax=10, epsilon=0.1)
+
+
+
