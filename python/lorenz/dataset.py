3a207bf53ca9109cb807bbfb6c1252bcf3b6445d
Xiaohan Zhang <xiaohanzhang.cmu@gmail.com>
2019-04-25 10:57:40 -0700

    Update on lorenz PDE solving

---
+++ python/lorenz/dataset.py
@@ -0,0 +1,48 @@
+""" Generate dataset for space sampling
+"""
+#def dataset(n_samples = 1<<15, batch_size=1<<5, 
+#            xmin=-1, xmax=1, zmin=-1, zmax=1, 
+#            sub_nx=16, sub_nz=16, patch_x=1., patch_z=1., 
+#            db_save_path='./'):
+
+import numpy as np
+
+def dataset(reuse=False, **args):
+    import os
+    # Check if database is reusable
+
+    xmin, xmax = args['xmin'], args['xmax']
+    zmin, zmax = args['zmin'], args['zmax']
+    sub_nx, sub_nz = args['sub_nx'], args['sub_nz']
+    patch_x, patch_z = args['patch_x'], args['patch_z'] 
+    n_samples = args['n_samples']
+    batch_size = args['batch_size']
+
+    db_file = args['db_save_path'] + 'open_cover_xmin_{0}_xmax_{1}_zmin_{2}_zmax_{3}_subnx_{4}_subnz_{5}_patchx_{6}_patch_z_{7}_batchsize_{8}_nsamples_{9}.npy'.format(xmin, xmax, zmin, zmax, sub_nx, sub_nz, patch_x, patch_z, batch_size, n_samples)
+
+    if reuse and os.path.exists(db_file):
+        db = np.load(db_file)
+        return db
+
+    def sample_domain(batch_size=32):
+        xs = np.random.uniform(xmin, xmax, batch_size)
+        zs = np.random.uniform(zmin, zmax, batch_size)
+        return np.array([np.stack(np.meshgrid(np.linspace(xi-.5*patch_x,xi+.5*patch_x,sub_nx),
+            np.linspace(zi-.5*patch_z, zi+.5*patch_z, sub_nz)), axis=-1)
+            for (xi,zi) in zip(xs,zs)])
+
+    db = []
+    print('batch_size = {0}'.format(batch_size))
+    for ind in range(n_samples):
+        X_sample = sample_domain(batch_size=batch_size)
+        db.append(X_sample)
+        if ind % 1000 == 0:
+            print('dataset generating .... {0}/{1}'.format(ind, n_samples))
+
+    db = np.array(db)
+    print('Database of shape: {0} saved to {1}'.format(db.shape, db_file))
+    np.save(db_file, db)
+
+    return db
+
+
--- python/lorenz/solve_lorenz_pde.py
+++ python/lorenz/solve_lorenz_pde.py
@@ -4,9 +4,14 @@
     2) Sample PDE domain patchwise
 
     Questions to resolve:
-    1) Run time shape dimension does not match?
-    2) Normalize input patch to have values [-1,1] for each channel?
-    3) Add laplacian regularizer to PDE.
+    1) Normalize input patch to have values [-1,1] for each channel?
+    2) Add laplacian regularizer to PDE.
+    3) Tune Adam, try other SGD optimizers, tune learning rate
+    4) Try deeper neural network
+    5) Initialize weights smarter ways
+    6) Parallel speeding up GPU or MPI
+    7) Prepare dataset beforehand, saving sampling time. Visualize each datapoint
+    8) Add adaptive sampling scheme to sample more patches from high loss region
 
     Found papers:
     1) https://arxiv.org/pdf/1708.07469.pdf essentially following the same steps
@@ -15,7 +20,8 @@ import sys
 from termcolor import colored, cprint
 import numpy as np
 import tensorflow as tf
-from model import model
+#from model import model
+from dataset import dataset
 
 def lr(epoch):
     learning_rate = 1e-3
@@ -30,28 +36,46 @@ def lr(epoch):
     return learning_rate
 
 
-def solve_lorenz_pde(xmin, xmax, zmin, zmax, epsilon, sigma, beta, gamma):
-    sub_nx, sub_nz = 16, 16
-    patch_x, patch_z = 1., 1.
+#def solve_lorenz_pde(n_samples, batch_size, 
+#                     xmin, xmax, zmin, zmax, 
+#                     sub_nx, sub_nz, patch_x, patch_z, 
+#                     db_save_path,
+#                     epsilon, sigma, beta, gamma):
 
-    x_ph = tf.placeholder(dtype=np.float64, shape=(None, sub_nx, sub_nz, 2), name='input')
+def solve_lorenz_pde(**args):
+
+    lr = args['lr'] # 1e-3  # learning rate
+    tol = 1e-9 # convergence tol
+    batch_size = args['batch_size'] # 1<<5
+    # n_batch = args['n_batch'] # 1<<6
+    epochs = args['epochs'] # 1<<20
+    sigma = args['sigma']
+    beta = args['beta']
+    gamma = args['gamma']
+
+    x_ph = tf.placeholder(dtype=np.float64, shape=(None, args['sub_nx'], args['sub_nz'], 2), name='input')
+
+#    def sample_domain(batch_size=32):
+#        xmin, xmax = args['xmin'], args['xmax']
+#        zmin, zmax = args['zmin'], args['zmax']
+#        sub_nx, sub_nz = args['sub_nx'], args['sub_nz']
+#        patch_x, patch_z = args['patch_x'], args['patch_z'] 
+#
+#        xs = np.random.uniform(xmin, xmax, batch_size)
+#        zs = np.random.uniform(zmin, zmax, batch_size)
+#        return np.array([np.stack(np.meshgrid(np.linspace(xi-.5*patch_x,xi+.5*patch_x,sub_nx),
+#            np.linspace(zi-.5*patch_z, zi+.5*patch_z, sub_nz)), axis=-1)
+#            for (xi,zi) in zip(xs,zs)])
 
     def mlp(x, hidden_sizes=(32,1), activation=tf.tanh, output_activation=None):
         for h in hidden_sizes[:-1]:
             x = tf.layers.dense(x, units=h, activation=activation)
         return tf.layers.dense(x, units=hidden_sizes[-1], activation=output_activation)
 
-    def sample_domain(batch_size=32):
-        xs = np.random.uniform(xmin, xmax, batch_size)
-        zs = np.random.uniform(zmin, zmax, batch_size)
-        return np.array([np.stack(np.meshgrid(np.linspace(xi-0.5*patch_x, xi+0.5*patch_x, sub_nx),
-            np.linspace(zi-0.5*patch_z, zi+0.5*patch_z, sub_nz)), axis=-1)
-            for (xi,zi) in zip(xs,zs)])
-
     def loss_function(inputs):
         xadv = tf.identity(inputs) # Such that dG/d_xadv = dG/d_x
-        #G = mlp(xadv, hidden_sizes=(32,1))
-        G, learning_rate = model(xadv)
+        G = mlp(xadv, hidden_sizes=(16,1))
+        #G, learning_rate = model(xadv)
         dG = tf.gradients(G, xadv)[0]
         # d2G,_ = tf.hessians(G, xadv) # Regularizer
         print('xadv, G and dG shape')
@@ -68,18 +92,16 @@ def solve_lorenz_pde(xmin, xmax, zmin, zmax, epsilon, sigma, beta, gamma):
                                     # + tf.math.scalar_mul(epsilon,(d2Gx+d2Gz))  # Regularizer
                                    )**2)
         print(loss_sum.shape)
-        return loss_sum, G, learning_rate
+        return loss_sum, G # , learning_rate
 
     """ Optimization setup
     """
-    #lr = 1e-3  # learning rate
-    tol = 1e-9 # convergence tol
-    batch_size = 1<<5
-    n_batch = 1<<6
-    epochs = 1<<20
+    # saver = tf.train.Saver()
 
-    loss_sum,G,learning_rate = loss_function(x_ph)
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.9,beta2=0.999,epsilon=1e-08)
+    # loss_sum,G,learning_rate = loss_function(x_ph)
+    loss_sum,G = loss_function(x_ph)
+    # optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.9,beta2=0.999,epsilon=1e-08)
+    optimizer = tf.train.AdamOptimizer(lr, beta1=0.9,beta2=0.999,epsilon=1e-08)
     grads_and_vars = optimizer.compute_gradients(loss_sum)
     op = optimizer.apply_gradients(grads_and_vars,global_step=tf.Variable(0, trainable=False))
 
@@ -87,15 +109,24 @@ def solve_lorenz_pde(xmin, xmax, zmin, zmax, epsilon, sigma, beta, gamma):
     sess.run(tf.global_variables_initializer())
     sess.run(tf.local_variables_initializer())
 
+
     for epoch in range(epochs): # Minimize loss_sum til |loss-prev_loss|<tol
 
-        for batch in range(n_batch):
-            # Sample a batch from cartesian envelope of domain
-            X_sample = sample_domain(batch_size)
-            # Feed X to x_ph, do optimization
-            result = sess.run([op, loss_sum, G], feed_dict={x_ph:X_sample, learning_rate:lr(epoch)})
+        db = dataset(reuse = False, **args)
+        inds = np.arange(db.shape[0])
+        np.random.shuffle(inds)
 
-        if epoch % 100 == 0:
+        for index, ind in enumerate(inds):
+            # Sample a batch from cartesian envelope of domain
+            # X_sample = sample_domain(batch_size)
+            # Feed X to x_ph, do optimization
+            #result = sess.run([op, loss_sum, G], feed_dict={x_ph:X_sample, learning_rate:lr(epoch)})
+            # result = sess.run([op, loss_sum, G], feed_dict={x_ph:X_sample})
+            result = sess.run([op, loss_sum, G], feed_dict={x_ph:db[ind]})
+            # if index % 100 == 0:
+            #     print("Index = {:5d}; Residuals={: 5.10E};".format(index, result[1]))
+            
+        if epoch % 1 == 0:
             print("Epoch = {:5d}; Residuals={: 5.10E};".format(epoch, result[1]))
             # Save model weights
             # ...
@@ -106,9 +137,31 @@ def solve_lorenz_pde(xmin, xmax, zmin, zmax, epsilon, sigma, beta, gamma):
 
     cprint("Failed to converged in {:d} epochs!!!".format(epoch), 'green', 'on_red')
 
+    # saver.save(sess, save_path=_SAVE_PATH, global_step=_global_step)
+
 if __name__ == '__main__':
-    solve_lorenz_pde(xmin=-10, xmax=10, zmin=-10, zmax=10, epsilon=0.1,
-            sigma=10, beta=8./3, gamma=25)
+    n_samples_ = 1<<10
+    batch_size_ = 1<<5
+    sub_nx_, sub_nz_ = 16, 16
+    patch_x_, patch_z_ = 1., 1.
+    xmin_, xmax_ = -10, 10
+    zmin_, zmax_ = -10, 10
+    db_save_path_ = './'
+    epsilon_ = 0.1
+    sigma_ = 10
+    beta_ = 8./3
+    gamma_ = 25
+    solve_lorenz_pde(n_samples=n_samples_, batch_size=batch_size_, 
+                     xmin=xmin_, xmax=xmax_, zmin=zmin_, zmax=zmax_, 
+                     sub_nx=sub_nx_, sub_nz=sub_nz_, patch_x=patch_x_, patch_z=patch_z_,
+                     db_save_path=db_save_path_,
+                     epsilon=epsilon_, sigma=sigma_, beta=beta_, gamma=gamma_, epochs=1<<128, lr = 1e-3)
+
+
+
+
+
+
 
 
 
