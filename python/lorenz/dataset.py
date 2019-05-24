59fcd29a7e780d193dabff876cf2c0c1e9054c01
Xiaohan Zhang <xiaohanzhang.cmu@gmail.com>
2019-05-24 14:46:19 -0700

    Add spherical sampling. Change cartesian dataset format

--- python/lorenz/dataset.py
+++ python/lorenz/dataset.py
@@ -1,6 +1,34 @@
 import os
 import numpy as np
 
+def dataset_sphere(reuse=False, **args):
+    """ Generate dataset for space sampling
+    """
+    R = args['R']
+    R0 = args['R0']
+    center = args['center']
+    n_samples = args['n_samples']
+    batch_size = args['batch_size']
+
+    db_file = args['db_save_path'] + 'open_cover_sphere_R_{}_Cx_{}_Cz_{}_batchsize_{}_nsamples_{}.npy'.format(R, center[0], center[1], batch_size, n_samples)
+
+    print('R= {0}, Cx = {1}, Cz = {2}, batchsize = {3}'.format(R, center[0], center[1], batch_size))
+
+    # Check if database is reusable
+    if reuse and os.path.exists(db_file):
+        return np.load(db_file)
+
+    def sample_domain(batch_size=32):
+        r  = np.sqrt(np.random.uniform(0, R, batch_size))
+        angle = np.pi * np.random.uniform(0, 2, batch_size)
+        xs = (r * np.cos(angle)+center[0]).reshape(-1,1)
+        zs = (r * np.sin(angle)+center[1]).reshape(-1,1)
+        return np.concatenate([xs,zs], axis=1)
+
+    db = np.array([sample_domain(batch_size) for _ in range(n_samples)])
+    np.save(db_file, db)
+    return db
+
 def dataset(reuse=False, **args):
     """ Generate dataset for space sampling
     """
@@ -20,10 +48,9 @@ def dataset(reuse=False, **args):
         return np.load(db_file)
 
     def sample_domain(batch_size=32):
-        xs = np.random.uniform(xmin, xmax, batch_size)
-        zs = np.random.uniform(zmin, zmax, batch_size)
-        return np.array([np.stack(np.meshgrid(np.linspace(xi-.5*patch_x,xi+.5*patch_x,sub_nx), np.linspace(zi-.5*patch_z, zi+.5*patch_z, sub_nz)), axis=-1)
-            for (xi,zi) in zip(xs,zs)])
+        xs = np.random.uniform(xmin, xmax, batch_size).reshape(-1,1)
+        zs = np.random.uniform(zmin, zmax, batch_size).reshape(-1,1)
+        return np.concatenate([xs,zs], axis=1)
 
     db = np.array([sample_domain(batch_size) for _ in range(n_samples)])
     np.save(db_file, db)
